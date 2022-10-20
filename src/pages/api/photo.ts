4657fff87db2c185140e547b1d28edf3354e8812
Atanas Tulilov <atanas.tulilov@ffwagency.com>
2022-10-20 09:17:57 +0300

    readme and page folder is moved to source folder

--- src/pages/api/photo.ts
+++ src/pages/api/photo.ts
@@ -6,7 +6,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<PhotoResponse>
 ) {
-  if (req.method === "POST") {
+  if (req.method !== "GET") {
     res.status(404).json({ error: "this method does not exist" });
     return;
   }
